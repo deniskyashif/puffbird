8edaf2ed8f9bf9bfdd7b836d32f63ddf1a8fb6ae
stinger907 <denis.kyashif@gmail.com>
2014-12-08 22:59:56 +0200

    added Gruntfile.js

---
+++ rest-api/Gruntfile.js
@@ -0,0 +1,32 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        express: {
+            options: {
+                port: 3030
+            },
+            dev: {
+                options: {
+                    script: 'bin/www'
+                }
+            }
+        },
+        watch: {
+            express: {
+                files: ['**/*.js'],
+                tasks: ['express:dev'],
+                options: {
+                    spawn: false 
+                }
+
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-express-server');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    grunt.registerTask('dev', [
+        'express:dev',
+        'watch'
+    ]);
+};
