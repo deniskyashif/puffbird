5e107fdc39b943d698c3fe6f457c0ed8a28b86ff
deniskyashif <denis.kyashif@gmail.com>
2015-01-28 00:06:40 +0200

    restricted feedback route access

--- config/routes.js
+++ config/routes.js
@@ -1,37 +1,37 @@
-var auth = require('./auth'),
-    controllers = require('../controllers');
+var auth = require('./auth')
+  , controllers = require('../controllers');
 
 module.exports = function(router) {
-    //auth
-    router.post('/login', auth.login);
-    router.post('/logout', auth.logout);
-    
-    // users
-    router.get('/api/users/:id', auth.isAuthenticated, controllers.users.getById);
-    router.get('/api/users', auth.isAuthenticated, controllers.users.getAll);
-    router.post('/api/users/', controllers.users.create);
-    router.put('/api/users/:id', auth.isAuthenticated, controllers.users.update);
+  //auth
+  router.post('/login', auth.login);
+  router.post('/logout', auth.logout);
 
-    //notes
-    router.get('/api/notes/:id', auth.isAuthenticated, controllers.notes.getById);
-    router.get('/api/notes', auth.isAuthenticated, controllers.notes.getAll);
-    router.post('/api/notes', auth.isAuthenticated, controllers.notes.create);
-    router.put('/api/notes/:id', auth.isAuthenticated, controllers.notes.update);
-    router.delete('/api/notes/:id', auth.isAuthenticated, controllers.notes.remove);
+  // users
+  router.get('/api/users/:id', auth.isAuthenticated, controllers.users.getById);
+  router.get('/api/users', auth.isAuthenticated, controllers.users.getAll);
+  router.post('/api/users/', controllers.users.create);
+  router.put('/api/users/:id', auth.isAuthenticated, controllers.users.update);
 
-    //feedback
-    router.get('/api/feedback/:id', controllers.feedback.getById);
-    router.get('/api/feedback', controllers.feedback.getAll);
-    router.post('/api/feedback', controllers.feedback.create);
-    router.delete('/api/feedback/:id', controllers.feedback.remove);
+  //notes
+  router.get('/api/notes/:id', auth.isAuthenticated, controllers.notes.getById);
+  router.get('/api/notes', auth.isAuthenticated, controllers.notes.getAll);
+  router.post('/api/notes', auth.isAuthenticated, controllers.notes.create);
+  router.put('/api/notes/:id', auth.isAuthenticated, controllers.notes.update);
+  router.delete('/api/notes/:id', auth.isAuthenticated, controllers.notes.remove);
 
-    router.get('/api/*', function(req, res) {
-        res.status(404);
-        res.end();
-    });
+  //feedback
+  router.get('/api/feedback/:id', auth.isAuthenticated, controllers.feedback.getById);
+  router.get('/api/feedback', auth.isAuthenticated, controllers.feedback.getAll);
+  router.post('/api/feedback', controllers.feedback.create);
+  router.delete('/api/feedback/:id', auth.isAuthenticated, controllers.feedback.remove);
 
-    //--application--
-    router.get('*', function(req, res) {
-        res.sendFile(__dirname + '/public/index.html');
-    });
+  router.get('/api/*', function(req, res) {
+    res.status(404);
+    res.end();
+  });
+
+  //--application--
+  router.get('*', function(req, res) {
+    res.sendFile(__dirname + '/public/index.html');
+  });
 };
