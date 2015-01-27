1d64a7cac7e09fae86a5c251b13e57b742316fcf
deniskyashif <denis.kyashif@gmail.com>
2015-01-27 23:27:39 +0200

    updated package.json

--- config/config.js
+++ config/config.js
@@ -1,16 +1,17 @@
 var DEV_PORT = 3030,
-    path = require('path'),
-    rootPath = path.normalize(__dirname + '/../');
+  path = require('path'),
+  rootPath = path.normalize(__dirname + '/../');
 
-module.exports = {
-    development: {
-        rootPath: rootPath,
-        db: 'mongodb://localhost/puffbird',
-        port: process.env.PORT || DEV_PORT
-    },
-    production: {
-        rootPath: rootPath,
-        db: 'mongodb://admin:puffbird@ds031681.mongolab.com:31681/puffbird',
-        port: process.env.PORT || DEV_PORT
-    }
+  var config = {
+    rootPath: rootPath,
+    port: process.env.PORT || DEV_PORT,
+    db: 'mongodb://admin:puffbird@ds031681.mongolab.com:31681/puffbird'
+  };
+
+module.exports = function(env) {
+  if (env === 'development') {  
+    config.db = 'mongodb://localhost/puffbird';
+  }
+
+  return config;
 };
